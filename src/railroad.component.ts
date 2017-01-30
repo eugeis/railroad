811dbeb2bb2736da17487b8b513208934b9f4b13
Jonas Möller <moeller-jonas@web.de>
2017-01-30 16:28:46 +0100

    Remove unnecessary background-grid declaration

--- src/railroad.component.ts
+++ src/railroad.component.ts
@@ -87,17 +87,6 @@ var svgNS = "http://www.w3.org/2000/svg";
 				(onContextMenu)="updateCtx($event)"
 				(onContextSelect)="handleCtx($event)"
 				(onResize)="updateSize($event)">
-				<svg:g class="background">
-					<svg:defs>
-						<svg:pattern id="smallGrid" width="10" height="10" patternUnits="userSpaceOnUse">
-							<svg:path d="M 10 0 L 0 0 0 10" fill="none" stroke="gray" stroke-width="0.5"/>
-						</svg:pattern>
-						<svg:pattern id="grid" width="100" height="100" patternUnits="userSpaceOnUse">
-							<svg:rect width="100" height="100" fill="url(#smallGrid)"/>
-							<svg:path d="M 100 0 L 0 0 0 100" fill="none" stroke="gray" stroke-width="1"/>
-						</svg:pattern>
-					</svg:defs>
-				</svg:g>
 
 				<svg:g *ngIf="timetable" class="partialTrips"
 					[pts]="timetable.partialTrips.all"
